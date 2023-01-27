4a5899259a31391110b934b273e755bb81b2919e
Administrator <Administrator@PLABDC01>
2023-01-26 20:04:11 -0800

    Add project files.

---
+++ MovieGuidePart1.py
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    movie_list = ['Monty Python and the Holy Grail',
+                  'On the Waterfront',
+                  'Cat on a Hot Roof']
+
+    display_menu()
+
+    while True:
+        command = input('Command: ')
+        if command.lower() == 'list':
+            list(movie_list)
+        elif command.lower() == 'add':
+            add(movie_list)
+        elif command.lower() == 'del':
+            delete(movie_list)
+        elif command.lower() == 'exit':
+            break
+        else:
+            print('Not a valid command. Please try again.\n')
+
+    print('Bye!')
+
+if __name__ == '__main__':
+    main()
+
+
