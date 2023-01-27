3bdc7819b254dce86de6443ed5d3f33c7043cb11
Administrator <Administrator@PLABDC01>
2023-01-27 15:09:04 -0800

    Gaynell Hooks CIS 210 Movie Guide Part 1

--- MovieGuidePart1.py
+++ MovieGuidePart1.py
@@ -3,30 +3,23 @@
 
 
 
+def list(movie_list):
+    for i, movie in enumerate(movie_list, start=1):
+        print(f'{i}. {movie}')
+    print()
 
+def add(movie_list):
+    movie = input('Name: ')
+    movie_list.append(movie)
+    print(f'{movie} was added.\n')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def delete(movie_list):
+        number = int(input('Number: '))
+        if number < 1 or number > len(movie_list):
+            print('Invalid movie number.\n')
+        else:
+            movie = movie_list.pop(number-1)
+            print(f'{movi} was deleted.\n')
 
 def main():
     movie_list = ['Monty Python and the Holy Grail',
